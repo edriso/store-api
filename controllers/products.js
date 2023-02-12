992c965b99a8334dd0c4e1a00e80f986a9bd4bff
edriso <edriso.dev@gmail.com>
2023-02-12 22:33:41 +0200

    filter by query param

--- controllers/products.js
+++ controllers/products.js
@@ -1,10 +1,32 @@
-const getProductsStatic = (req, res) => {
-  throw new Error('Whaa!');
-  // res.status(200).json({ msg: 'products static route' });
+const Product = require('../models/product');
+
+const getProductsStatic = async (req, res) => {
+  const products = await Product.find(
+    {
+      featured: true,
+      company: 'caressa',
+    },
+    null,
+    { strict: false }
+  );
+
+  // throw new Error('Whaa!');
+  res.status(200).json({ products, results: products.length });
 };
 
-const getAllProducts = (req, res) => {
-  res.status(200).json({ msg: 'products route' });
+const getAllProducts = async (req, res) => {
+  const queryObject = {};
+  const { featured } = req.query;
+
+  if (featured) {
+    queryObject.featured = featured === 'true' ? true : false;
+  }
+
+  const products = await Product.find(queryObject).setOptions({
+    strict: false,
+  });
+
+  res.status(200).json({ results: products.length, products });
 };
 
 module.exports = {
