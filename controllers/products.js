92cb9b3c018078808ee4e540225cfe38f695d0de
edriso <edriso.dev@gmail.com>
2023-02-12 12:17:29 +0200

    add another route

--- controllers/products.js
+++ controllers/products.js
@@ -1,7 +1,12 @@
+const getProductsStatic = (req, res) => {
+  res.status(200).json({ msg: 'products static route' });
+};
+
 const getAllProducts = (req, res) => {
-  res.status(200).json({ msg: 'Products Array' });
+  res.status(200).json({ msg: 'products route' });
 };
 
 module.exports = {
+  getProductsStatic,
   getAllProducts,
 };
--- routes/products.js
+++ routes/products.js
@@ -5,4 +5,6 @@ const productsController = require('../controllers/products');
 
 router.route('/').get(productsController.getAllProducts);
 
+router.route('/static').get(productsController.getProductsStatic);
+
 module.exports = router;
