a807a27dcc4810be2145a5f2a23fbf793d78e7b4
edriso <edriso.dev@gmail.com>
2023-02-13 16:38:27 +0200

    sort products

--- controllers/products.js
+++ controllers/products.js
@@ -10,7 +10,7 @@ const getProductsStatic = async (req, res) => {
 const getAllProducts = async (req, res) => {
   const queryObject = {};
 
-  const { featured, company, name } = req.query;
+  const { featured, company, name, sort } = req.query;
 
   if (featured) {
     queryObject.featured = featured === 'true' ? true : false;
@@ -23,9 +23,14 @@ const getAllProducts = async (req, res) => {
     queryObject.name = { $regex: name, $options: 'i' };
   }
 
-  const products = await Product.find(queryObject).setOptions({
-    strict: false,
-  });
+  let result = Product.find(queryObject);
+  if (sort) {
+    const sortList = sort.split(',').join(' ');
+    result = result.sort(sortList);
+  } else {
+    result = result.sort('createdAt');
+  }
+  const products = await result;
 
   res.status(200).json({ results: products.length, products });
 };
